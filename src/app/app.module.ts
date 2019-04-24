00524c1f69766f2a90785f511cfd8050f870b954
zhaishuo <1371007696@qq.com>
2019-04-25 04:56:35 +0800

    使用ngx-admin搭建SmartTeamUI 前台代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ModalModule} from "ngx-bootstrap";
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './@core/core.module';
+
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { AdminModule } from "./component/index/admin/admin.module";
+import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './@theme/theme.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-
+  declarations: [AppComponent],
   imports: [
-    AdminModule,
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    ModalModule,
-    ModalModule.forRoot()
+
+    NgbModule.forRoot(),
+    ThemeModule.forRoot(),
+    CoreModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/typing.d.ts
+++
@@ -1,1 +0,0 @@
-declare var tinymce: any;
