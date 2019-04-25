f71ebb06d920f22c200944966290e2a60b08d2a4
zhaishuo <1371007696@qq.com>
2019-04-26 05:50:17 +0800

    change default theme and footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">翟硕</a></b> 2019</span>
     <div class="socials">
     </div>
   `,
