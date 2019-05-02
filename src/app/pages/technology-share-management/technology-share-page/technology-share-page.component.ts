f04b040bee323f1db2da78e7d9be3a0b2d923c4a
zhaishuo <1371007696@qq.com>
2019-05-03 07:50:10 +0800

    test infinite list

--- src/app/pages/technology-share-management/technology-share-page/technology-share-page.component.ts
+++ src/app/pages/technology-share-management/technology-share-page/technology-share-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NewsService} from '../services/news.service';
 
 @Component({
   selector: 'ngx-technology-share-page',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologySharePageComponent implements OnInit {
 
-  constructor() { }
+  firstCard = {
+    news: [],
+    placeholders: [],
+    loading: false,
+    pageToLoadNext: 1,
+  };
+  secondCard = {
+    news: [],
+    placeholders: [],
+    loading: false,
+    pageToLoadNext: 1,
+  };
+  pageSize = 10;
+
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
+  }
+
+  loadNext(cardData) {
+    if (cardData.loading) { return; }
+
+    cardData.loading = true;
+    cardData.placeholders = new Array(this.pageSize);
+    this.newsService.load(cardData.pageToLoadNext, this.pageSize)
+      .subscribe(nextNews => {
+        cardData.placeholders = [];
+        cardData.news.push(...nextNews);
+        cardData.loading = false;
+        cardData.pageToLoadNext++;
+      });
   }
 
 }
--- src/app/pages/technology-share-management/technology-share-page/technology-share-page.module.ts
+++ src/app/pages/technology-share-management/technology-share-page/technology-share-page.module.ts
@@ -3,6 +3,9 @@ import {ThemeModule} from '../../../@theme/theme.module';
 import {CommonModule} from '@angular/common';
 import {TechnologySharePageRoutingModule} from './technology-share-page-routing.module';
 import {TechnologySharePageComponent} from './technology-share-page.component';
+import {NewsPostComponent} from './news-post/news-post.component';
+import {NewsPostPlaceholderComponent} from './news-post-placeholder/news-post-placeholder.component';
+import {NewsService} from '../services/news.service';
 
 @NgModule({
   imports: [
@@ -10,7 +13,12 @@ import {TechnologySharePageComponent} from './technology-share-page.component';
     CommonModule,
     TechnologySharePageRoutingModule,
   ],
-  declarations: [TechnologySharePageComponent],
+  declarations: [
+    TechnologySharePageComponent,
+    NewsPostComponent,
+    NewsPostPlaceholderComponent,
+  ],
+  providers: [NewsService],
 })
 export class TechnologySharePageModule {
 
