8f5f643ce013a990b61c633c1b3babf5c4bc131a
zhaishuo <1371007696@qq.com>
2019-05-03 06:11:13 +0800

    reset password cancel back to dashboard

--- src/app/pages/reset-password/reset-password-page/reset-password-page.component.ts
+++ src/app/pages/reset-password/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-reset-password-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+  cancel() {
+    this.route.navigate(['/pages/dashboard-management/dashboard-page']);
+  }
 }
