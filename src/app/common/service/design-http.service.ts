2806bb56563a65e143fb158c6d74eba4189b85e2
zhaishuo <1371007696@qq.com>
2019-05-06 04:49:28 +0800

    add service file

---
+++ src/app/common/service/design-http.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable()
+export class DesignHttpService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json;application/x-www-form-urlencodeed; charset=utf-8'}),
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+}
