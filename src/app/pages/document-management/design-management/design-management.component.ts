1d186da1e23371038674bccc086bca454f168f02
zhaishuo <1371007696@qq.com>
2019-05-09 23:56:54 +0800

    update page

--- src/app/pages/document-management/design-management/design-management.component.ts
+++ src/app/pages/document-management/design-management/design-management.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignManagementComponent implements OnInit {
 
+  private designList: Array<any>;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/document-management/requirement-management/requirement-management.component.ts
+++ src/app/pages/document-management/requirement-management/requirement-management.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequirementManagementComponent implements OnInit {
 
+  private requirementList: Array<any>;
+
   constructor() { }
 
   ngOnInit() {
